5bb6f3c81347253df1cb552373b7cf9480783258
VaneSuazoU <vsuazo.93@gmail.com>
2021-06-24 12:52:54 -0400

    Creacion paginas

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 import './global.css';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badge />, container)
+ReactDOM.render(<BadgeNew />, container)
